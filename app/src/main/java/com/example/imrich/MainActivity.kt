6340bb9b6fa3579c1d44fb522e09b1a801658a4a
Marielle-ux <vajnbergtomiris@gmail.com>
2025-01-08 20:44:54 +0500

    echo "# MediaPlayer" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Marielle-ux/MediaPlayer.git git push -u origin main

--- app/src/main/java/com/example/imrich/MainActivity.kt
+++ app/src/main/java/com/example/imrich/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.imrich
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,28 +17,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var change = true
-        binding.btnChange.setOnClickListener {
-            if (change) {
-                binding.imgChangeView.setImageResource(R.mipmap.ic_launcher_round)
-                change = false
-            } else {
-                binding.imgChangeView.setImageResource(R.mipmap.ic_launcher)
-                change = true
+        val mediaDo = MediaPlayer.create(this, R.raw.zvuk_do)
+        val mediaRe = MediaPlayer.create(this, R.raw.zvuk_re)
+        val mediaMi = MediaPlayer.create(this, R.raw.zvuk_mi)
+        val mediaFa = MediaPlayer.create(this, R.raw.zvuk_fa)
+        val mediaSol = MediaPlayer.create(this, R.raw.zvuk_sol)
+        val mediaLya = MediaPlayer.create(this, R.raw.zvuk_lya)
+        val mediaSi = MediaPlayer.create(this, R.raw.zvuk_si)
 
+        binding.btnDo.setOnClickListener {
+            mediaDo.start()
+        }
 
-                enableEdgeToEdge()
-                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                    v.setPadding(
-                        systemBars.left,
-                        systemBars.top,
-                        systemBars.right,
-                        systemBars.bottom
-                    )
-                    insets
-                }
-            }
+        binding.btnRe.setOnClickListener {
+            mediaRe.start()
+        }
+
+        binding.btnMi.setOnClickListener {
+            mediaMi.start()
+        }
+
+        binding.btnFa.setOnClickListener {
+            mediaFa.start()
+        }
+
+        binding.btnSol.setOnClickListener {
+            mediaSol.start()
+        }
+
+        binding.btnLya.setOnClickListener {
+            mediaLya.start()
+        }
+
+        binding.btnSi.setOnClickListener {
+            mediaSi.start()
         }
     }
 }
+
